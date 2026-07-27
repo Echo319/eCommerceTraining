0522008f6c083b1078e48da7db913321b20563ec
Robin Davies <r.davies319@gmail.com>
2026-07-27 22:30:09 +0100

    Initial commit
    Mono repo with all the modules and known dependencies
    
    Used AI to gen a docker compose file and it recommeded a application yml to go with it.

---
+++ ProductCatalog/src/main/java/com/rdavies/productcataloge/ProductCatalogeApplication.java
@@ -0,0 +1,13 @@
+package com.rdavies.productcataloge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogeApplication.class, args);
+    }
+
+}
---
+++ ProductCatalog/src/test/java/com/rdavies/productcataloge/ProductCatalogeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdavies.productcataloge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
