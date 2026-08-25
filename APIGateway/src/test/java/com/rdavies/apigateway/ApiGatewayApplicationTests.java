e23ad8503b6aa84390c82c820ccecae413e6cb58
Robin Davies <r.davies319@gmail.com>
2026-08-25 14:49:05 +0100

    Got swagger UI working via the power of AI

--- APIGateway/src/test/java/com/rdavies/apigateway/ApiGatewayApplicationTests.java
+++ APIGateway/src/test/java/com/rdavies/apigateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,27 @@ package com.rdavies.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiGatewayApplicationTests {
 
+    @Autowired
+    private RouteDefinitionLocator routeDefinitionLocator;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void productCatalogDocsRouteIsRegistered() {
+        StepVerifier.create(routeDefinitionLocator.getRouteDefinitions()
+                .filter(routeDefinition -> "product-service-docs".equals(routeDefinition.getId()))
+                .hasElements())
+            .expectNext(true)
+            .verifyComplete();
     }
 
 }
